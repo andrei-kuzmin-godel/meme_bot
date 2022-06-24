87512afc6e8a14d0a688d9671b2824cbd60e4915
Andrei Kuzmin <andrei.kuzmin@godeltech.com>
2022-06-24 15:30:53 +0200

    removed logs

--- getNumberOfMemesRequested.js
+++ getNumberOfMemesRequested.js
@@ -1,6 +1,5 @@
 module.exports = message => {
   const receivedTextMessage = String(message);
-  console.log(typeof Number(receivedTextMessage))
 
   return isNaN(receivedTextMessage) ? 1 : parseInt(receivedTextMessage, 10);
 }
